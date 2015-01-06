bd118c43bff73e36100471536e6f52407664d2de
Andrew Farmer <adf@ncgr.org>
2015-01-06 14:59:11 -0600

    removed "gene family stuff"

--- theme/templates/tripal_gene_description.tpl.php
+++ theme/templates/tripal_gene_description.tpl.php
@@ -4,8 +4,6 @@ $gene_id = $variables['node']->feature->feature_id;
 
 $result = db_query('SELECT description FROM chado.gene WHERE gene_id = :uid', array(':uid' => $gene_id));
 
-echo "<b>gene family stuff</b><br>";
-
 
 foreach($result as $value){
   print $value->description;
