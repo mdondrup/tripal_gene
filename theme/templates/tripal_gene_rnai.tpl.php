492a5403760242504e356f0354e95ca079eef671
mdondrup <mdondrup@gmail.com>
2018-06-04 10:36:31 +0200

    Added the RNAi template, that will integrate into a chado feature page.

---
+++ theme/templates/tripal_gene_rnai.tpl.php
@@ -0,0 +1,7 @@
+<?php
+
+  // embedd the pre-defined block view to add the associated RNAi for this chado feature
+  echo views_embed_view('rnai_experiments_block_view', 'rnai_block_feature', $node->nid);
+
+ 
+
